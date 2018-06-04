b18e7395df85977e292326c1d721edcf3300bd71
Ibnul Jahan <ijahan1@stuy.edu>
2018-06-04 14:05:08 -0400

    couple of things

--- wire.js
+++ wire.js
@@ -16,32 +16,71 @@ svg.appendChild(line);
 
 */
 
-var svg = document.getElementById("vimage");
+//e.clientX this is for the screen
+//e.offsetX this is for the svg
 
+
+var svg = document.getElementById("vimage");
+var height = svg.getAttribute('height');
+var width = svg.getAttribute('width');
+
+
+svg.addEventListener("click", function(e){console.log(e.offsetX, e.offsetY);} );
 
 
 //var vector = {xmag:"500", color:"white"};
 var vector_field;
 
-var makeVector = function(xmag, ymag) {
-    return {xmag: xmag, ymag: ymag}
+var makeVector = function(xcor, ycor, xmag, ymag) {
+    return {xcor: xcor,
+	    ycor: ycor,
+	    xmag: xmag,
+	    ymag: ymag,
+	    addtoY: function(newymag){ ymag += newymag; },
+	    addtoX: function(newxmag){ xmag += newxmag; },
+	    addVector : function(newX, newY){ addtoX(newX); addtoY(newY); },
+	    display : function() {
+		var cir = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+		cir.setAttribute("cx", xcor);
+		cir.setAttribute("cy", ycor);
+		cir.setAttribute("r", 10);
+		cir.setAttribute("fill", "blue");
+		cir.setAttribute("stroke", "black");
+		//so far this next line just prints the xcor of the vector
+		cir.setAttribute("onclick", 'console.log(' + xcor + ')' );
+		svg.appendChild(cir)
+	    }
+	   }
 };
 
 var makeVectorField = function(cols, rows) {
     vector_field = new Array(cols);
     var i;
-    for (i = 0; i < vector_field.length; i++) { 
+    for (i = 0; i < vector_field.length; i++) {
+	xincrement = width / vector_field.length
 	vector_field[i] = new Array(rows);
 	var j;
 	for (j = 0; j < vector_field[i].length; j++){
-	    vector_field[i][j] = makeVector(0,0)
+	    yincrement = height / vector_field[i].length;
+	    vector_field[i][j] = makeVector(xincrement * i, height - (yincrement * j), 0,0)
 	}
     }
 };
 
-makeVectorField(10, 20);
+makeVectorField(20, 10);
 console.log(vector_field);
 
+var displayVectors = function(){
+    var i;
+    for (i = 0; i < vector_field.length; i++) {
+	var j;
+	for (j = 0; j < vector_field[i].length; j++){
+	    vector_field[i][j].display()
+	}
+    }
+}
+displayVectors();
+	
 
 /////////////////FOR THE SLIDER
 var slider = document.getElementById("myRange");
